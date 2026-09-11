7201def1bdd70bfea654b33c6645859e8ea04613
Vishnu Varma <vishnupvarma@gmail.com>
2026-09-11 16:32:39 -0400

    Preparing for azure deployment

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,10 @@
 import os
 
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-load_dotenv() 
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
